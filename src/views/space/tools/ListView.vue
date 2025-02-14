49d90083d7f562af27ef69903849588b22fd6668
hahaliu1029 <1853991699@qq.com>
2025-02-14 15:59:30 +0800

    添加多个页面视图组件，移除不必要的图标组件

---
+++ src/views/space/tools/ListView.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts"></script>
+
+<template>
+  <p>自定义API插件列表页面</p>
+</template>
+
+<style scoped></style>
---
+++ src/views/store/apps/ListView.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts"></script>
+
+<template>
+  <p>应用广场页面列表</p>
+</template>
+
+<style scoped></style>
